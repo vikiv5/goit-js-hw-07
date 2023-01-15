1a74e983d024ffd52a2cc55743502aa526fd5c18
vikiv5 <vi_kov@ukr.net>
2023-01-15 16:17:07 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const imgList = document.querySelector ('.gallery'); 
+const imgMarkup = createImagesMarkup (galleryItems);
+imgList.insertAdjacentHTML('beforeend', imgMarkup);
+
+ function createImagesMarkup (galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+       return ` <li><a class = "gallery__item " href="${original}">
+       <img 
+       class = "gallery__image" 
+       src = "${preview}"
+       alt = "${description}"/>
+       </a></li>`;
+    }) .join ("");
+ 
+ }
+ //new SimpleLightbox(".gallery a");
+ const gallery = new SimpleLightbox ('.gallery a', 
+ {
+    captions : true ,
+    captionsData : "alt", 
+    captionDelay:250,
+ }) ;
